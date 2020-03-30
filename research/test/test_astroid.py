19fbb1e722f39272df36b32203604313a998dd96
git <820472580@qq.com>
2020-03-31 00:19:02 +0800

    更新 astor 遍历

--- research/test/test_astroid.py
+++ research/test/test_astroid.py
@@ -1,10 +1,9 @@
 import sys
-MODULE = r"D:\Users\82047\Desktop\test"
+MODULE = r"D:\Users\82047\Desktop\repo\QtConfig\QMVVM\_vender"
 if MODULE not in sys.path:
     sys.path.append(MODULE)
-
-from astroid import parse
-module = parse('''
+import astroid
+module = astroid.extract_node('''
 def func(first, second):
     return first + second
 
@@ -13,4 +12,39 @@ arg_2 = 3
 func(arg_1, arg_2)
 ''')
 
-print module.body[-1]
+
+# print(module)
+
+
+def format_to_fstring_transform(node):
+    
+    f_string_node = astroid.JoinedStr(
+        lineno=node.lineno,
+        col_offset=node.col_offset,
+        parent=node.parent,
+    )
+    formatted_value_node = astroid.FormattedValue(
+        lineno=node.lineno,
+        col_offset=node.col_offset,
+        parent=node.parent,
+    )
+    formatted_value_node.postinit(value=node.args[0])
+
+    # Removes the {} since it will be represented as
+    # formatted_value_node
+    string = astroid.Const(node.func.expr.value.replace('{}', ''))
+
+    print node
+    f_string_node.postinit(values=[string, formatted_value_node])
+    node.keywords = "a"
+    return astroid.Const(node.func.expr.value.replace('{}', ''))
+
+astroid.MANAGER.register_transform(
+    astroid.Call,
+    format_to_fstring_transform,
+)
+
+tree = astroid.parse('''
+"my name is {}".format(name)
+''')
+print(tree.as_string())
